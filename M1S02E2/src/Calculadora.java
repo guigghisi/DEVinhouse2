e1e340048ca64ef767bc8698a546719b58767f3d
Guigghisi <guilhermeghisi14@gmail.com>
2022-10-10 15:50:58 -0300

    M2S02E2

---
+++ M1S02E2/src/Calculadora.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        var numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
+        var numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero"));
+        var operador = JOptionPane.showInputDialog("Digite o operador");
+        switch (operador) {
+            case "-" -> System.out.println(numero1 - numero2);
+            case "+" -> System.out.println(numero1 + numero2);
+            case "/" -> System.out.println(numero1 / numero2);
+            case "*" -> System.out.println(numero1 * numero2);
+            default -> System.out.println("Operador incorreto");
+        }
+    }
+
+}
