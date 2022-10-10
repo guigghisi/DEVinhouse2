c7cff6aa7b9deb075d8c56196a7222522b1a193d
Guigghisi <guilhermeghisi14@gmail.com>
2022-10-10 16:47:25 -0300

    M2S02E3

---
+++ M1S02E3/src/CalculadoraLoop.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+public class CalculadoraLoop {
+    public static void main(String[] args) {
+        var numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
+        var numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero"));
+        int resultado = 1;
+        var operador = JOptionPane.showInputDialog("Digite o operador");
+
+        switch (operador) {
+            case "-" -> resultado = (numero1 - numero2);
+            case "+" -> resultado = (numero1 + numero2);
+            case "/" -> resultado = (numero1 / numero2);
+            case "*" -> resultado = (numero1 * numero2);
+            default -> System.out.println("Operador incorreto");
+        }
+        while (!operador.equalsIgnoreCase("fim")) {
+            operador = JOptionPane.showInputDialog("Digite o operador");
+
+            switch (operador) {
+                case "-" -> {
+                    numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero"));
+                    resultado = (resultado - numero2);
+                }
+                case "+" -> {
+                    numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero"));
+                    resultado = (resultado + numero2);
+                }
+                case "/" -> {
+                    numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero"));
+                    resultado = (resultado / numero2);
+                }
+                case "*" -> {
+                    numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero"));
+                    resultado = (resultado * numero2);
+                }
+                case "fim" -> System.out.println("Resultado:" + resultado);
+                default -> System.out.println("Operador incorreto");
+            }
+        }
+    }
+}
