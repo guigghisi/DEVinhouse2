fde0cf612eb4b4b879602d73c73cda8a5dc043eb
Guigghisi <guilhermeghisi14@gmail.com>
2022-10-10 15:26:43 -0300

    M2S02E1

---
+++ M1S02E1/src/Soma.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Soma {
+    public static void main(String[] args) {
+        var numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
+        var numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número"));
+        System.out.println("A some é igual a: " + (numero1 + numero2));
+    }
+}
