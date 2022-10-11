d8b0cf1616c306db8534eaa7b6d99d6f8bf6741d
Guigghisi <guilhermeghisi14@gmail.com>
2022-10-10 21:26:25 -0300

    M1S02E7

---
+++ M1S02E7/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int b = 1 / 0;
+
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
