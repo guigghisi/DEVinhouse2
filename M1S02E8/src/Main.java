0af97beb1f288e6ba523e44f8db5bc6d4a0985ca
Guigghisi <guilhermeghisi14@gmail.com>
2022-10-10 21:29:39 -0300

    M1S02E8

---
+++ M1S02E8/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String nome;
+            ArrayList<String> nomes = new ArrayList<>();
+            do {
+                nome = JOptionPane.showInputDialog("Digite o nome da pessoa");
+                nomes.add(nome);
+            } while (!nome.equals("fim"));
+            Collections.sort(nomes);
+            nomes.forEach(e -> JOptionPane.showMessageDialog(null, e));
+        } catch (NullPointerException exception) {
+            System.out.println(exception);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+    }
+}
